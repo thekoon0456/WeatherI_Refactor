1f80b631058204eca58edc8056d5ba5d364f0d44
thekoon0456 <thekoon0456@gmail.com>
2023-09-18 00:42:33 +0900

    [Fix] Widget Retry 함수 수정

--- AlarmWeather/WidgetExtension/Extension.swift
+++ AlarmWeather/WidgetExtension/Extension.swift
@@ -8,27 +8,15 @@
 import Foundation
 
 protocol RetryRequest {
-    func retryRequest<T>(completion: @escaping (Result<[T], NetworkError>) -> Void, retryCount: Int)
+    func retryRequest<T>(completion: @escaping (Result<[T], NetworkError>) -> Void)
     func performRequest<T>(completion: @escaping (Result<[T], NetworkError>) -> Void)
     func setCustomURLSession(retryRequest: Double) -> URLSession
 }
 
 extension RetryRequest {
-    //통신 실패시 1초 뒤에 통신 재시도 코드
-    func retryRequest<T>(completion: @escaping (Result<[T], NetworkError>) -> Void, retryCount: Int = 0) {
-        guard retryCount < 10 else {
-            fatalError("통신 재시도 10회 초과로 앱을 종료합니다.")
-        }
-        
+    func retryRequest<T>(completion: @escaping (Result<[T], NetworkError>) -> (Void)) {
         DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
-            performRequest { (result: Result<[T], NetworkError>) in
-                switch result {
-                case .success:
-                    completion(result)
-                case .failure:
-                    self.retryRequest(completion: completion, retryCount: retryCount + 1)
-                }
-            }
+            performRequest(completion: completion)
         }
     }
 
